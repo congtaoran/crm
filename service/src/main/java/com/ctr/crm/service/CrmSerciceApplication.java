bcd6812cfd22bc38a3613afced3a38f9d34a13e0
congtr <81727937@qq.com>
2023-01-16 10:17:37 +0800

    配置pom，启动项

---
+++ service/src/main/java/com/ctr/crm/service/CrmSerciceApplication.java
@@ -0,0 +1,13 @@
+package com.ctr.crm.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class CrmSerciceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrmSerciceApplication.class, args);
+    }
+}
